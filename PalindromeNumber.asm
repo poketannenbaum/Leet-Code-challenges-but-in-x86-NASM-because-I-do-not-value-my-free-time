f9f0d94524f2c9a65dea380123b0eb160341bf3b
David Sardonini <155016041+poketannenbaum@users.noreply.github.com>
2024-07-15 14:36:02 -0400

    Create PalindromeNumber.asm

---
+++ PalindromeNumber.asm
@@ -0,0 +1,41 @@
+section .data
+    prompt1 db 'Enter number',0
+    prompt2 db 'Not a palindrome',0
+    prompt3 db 'Palindrome',0
+    basenum dd 0
+    
+section .text
+global main
+extern io_print_string, io_get_dec
+    main:
+        mov esi, 0
+        mov eax, prompt1
+        call io_print_string
+        mov eax, basenum
+        call io_get_dec
+        mov ebx, basenum
+        mov [ebx], eax
+    readd:
+        xor ebx, ebx
+        xor edx, edx
+        mov ebx, 10
+        div ebx
+        add esi, edx
+        imul esi, 10
+        cmp eax, 10
+        jle processing
+        jmp readd
+    processing:
+        add esi, eax
+        mov ebx, basenum
+        cmp [ebx], esi
+        je palindrome
+        mov eax, prompt2
+        call io_print_string
+        jmp end
+    palindrome:
+        mov eax, prompt3
+        call io_print_string
+        jmp end
+    end:
+        ret 1
